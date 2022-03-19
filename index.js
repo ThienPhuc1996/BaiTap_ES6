8fb03d28cc7f942057112f74092b46c9c8520c39
unknown <phamtranthienphuc1996@gmail.com>
2022-03-19 10:38:27 +0700

    BaiTap_ES6

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+document.querySelector('#signup-form .action ul li :last-child a').onclick = function(){
+    let btnSubmit = document.querySelector('#signup-form .action ul li :last-child a');
+    alert('1234');
+    if (btnSubmit.innerHTML.toLowerCase == 'Finish') {
+        alert('123');
+    }
+    
+    
+}
+*/
