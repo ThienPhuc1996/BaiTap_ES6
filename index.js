e9454047afea4595507e2d3ab872b9562b2b3883
unknown <phamtranthienphuc1996@gmail.com>
2022-03-22 12:37:15 +0700

    Baitap_ES6_complete

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-/*
-document.querySelector('#signup-form .action ul li :last-child a').onclick = function(){
-    let btnSubmit = document.querySelector('#signup-form .action ul li :last-child a');
-    alert('1234');
-    if (btnSubmit.innerHTML.toLowerCase == 'Finish') {
-        alert('123');
+
+document.querySelector('#signup-form .actions ul li:last-child a').onclick = function(){
+ this.append
+    
+    //Tổ chức DOM es6 tiếp tục để lấy input từ user
+
+    var arrThongTin = document.querySelectorAll('#signup-form input', '#signup-form select');
+
+    let thongTin ={};
+    for (let input of arrThongTin){
+        let id = input.id;
+        let value = input.value;
+        thongTin = {...thongTin, [id]:value}
     }
-    
-    
+
+let contentHTML = '';
+for (let key in thongTin){
+   contentHTML += `
+   <tr>
+      <td> ${key} </td>
+      <td> ${thongTin[key]} </td>
+   </tr>
+   `
 }
-*/
+document.querySelector('#tblThongTin').innerHTML = contentHTML;
+}
+
+
